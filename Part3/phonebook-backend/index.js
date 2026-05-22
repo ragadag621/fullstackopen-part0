b74434b96993142ab6d78864bd0eead15afc6adb
ragadag621 <ragadag62@gmail.com>
2026-05-22 15:59:22 +0300

    exercises 3.15-3.18: implement database delete, put route, and centralized error handler

--- Part3/phonebook-backend/index.js
+++ Part3/phonebook-backend/index.js
@@ -1,13 +1,13 @@
-
-require('dotenv').config()
 const express = require("express")
 const app = express()
+require('dotenv').config()
 const morgan = require('morgan')
 const cors = require('cors')
 app.use(cors())
-app.use(express.json())
-app.use(express.static('dist'))
 const Person = require('./models/person')
+app.use(express.static('dist'))
+app.use(express.json())
+
 
 morgan.token('body',(req,res)=>{
   if(req.method==='POST'){
@@ -21,31 +21,14 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 //------------Routes----------------
 
-app.get("/info", (req, res) => {  
-  Person.countDocuments({})
-    .then(count => {
-      const info = `Phonebook has info for ${count} people`
-      const date = new Date()
-      res.send(`${info}<br><br>${date}`)
-    })
-    .catch(error => {
-      console.log(error)
-      res.status(500).send({ error: 'internal server error' })
-    }) 
-})   
-
-
-app.get("/api/persons", (req, res) => {
+app.get("/api/persons", (req, res,next) => {
   Person.find({}).then(persons => {
     res.json(persons)
   })
-    .catch(error => {
-      console.log(error)
-      res.status(500).send({ error: 'internal server error' })
-    })
+    .catch(error => next(error))
 })
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res,next) => {
   Person.findById(req.params.id)
     .then(person => {
       if (person) {
@@ -54,26 +37,11 @@ app.get("/api/persons/:id", (req, res) => {
         res.status(404).end()
       }
     })
-    .catch(error => {
-      console.log(error)
-      res.status(400).send({ error: 'wrong id' })
-    })
+    .catch(error => next(error))
+
 })
 
-app.delete("/api/persons/:id", (req, res) => {
-  Person.findByIdAndDelete(req.params.id)
-    .then(result => {
-      res.status(204).end()
-    })
-    .catch(error => {
-      console.log(error)
-      res.status(400).send({ error: 'wrong   id' })
-    })
-})
-  
-
-
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res,next) => {
   const body = req.body
 
   if (!body.name || !body.number) {
@@ -95,17 +63,59 @@ app.post("/api/persons", (req, res) => {
         console.log(`added ${savedPerson.name} to phonebook`)
         res.json(savedPerson) 
       })
-      .catch(error => {
-        console.log(error)
-        res.status(500).json({ error: 'failed to save' })
-      })
+      .catch(error => next(error))
   })
 })
+
+app.put("/api/persons/:id", (req, res,next) => {
+  const body = req.body
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+  Person.findByIdAndUpdate(req.params.id, person, { new: true , runValidators: true, context: 'query' })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+   .catch(error => next(error))      
+})
+
+app.delete("/api/persons/:id", (req, res,next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+
+})
+  
+app.get("/info", (req, res,next) => {  
+  Person.countDocuments({})
+    .then(count => {
+      const info = `Phonebook has info for ${count} people`
+      const date = new Date()
+      res.send(`${info}<br><br>${date}`)
+    })
+    .catch(error => next(error))
+})
+
 
 const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
+
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 3001
